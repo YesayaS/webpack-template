7e699871e14be48f93e3d17eb37710fecbb80d88
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-09-26 12:25:44 +0700

    Add tailwind & change babel config

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import {} from "./app/app.js";
+import "./app/app.css";
 
 export const isTrue = true;
+const hlink = document.createElement("a");
+hlink.textContent = "this is an example";
+
+const body = document.body;
+body.appendChild(hlink);
